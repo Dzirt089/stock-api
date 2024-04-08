d1e7a877b2338382c828f13e6dd890483e83ffa4
Илья Емец <suoza089@gmail.com>
2024-04-08 20:59:46 +0300

    Тестовые пробы фильтра, мидллваре и т.п.

---
+++ src/OzonEdu.StockApi/Configuration/Middlewares/RequestLoggingMiddleware.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Identity.Data;
+using System.Text;
+
+namespace OzonEdu.StockApi.Configuration.Middlewares
+{
+    public class RequestLoggingMiddleware
+    {
+        private readonly ILogger<RequestLoggingMiddleware> _logger;
+        private readonly RequestDelegate _next;
+        public RequestLoggingMiddleware(RequestDelegate next, ILogger<RequestLoggingMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            await LogRequest(context);
+            await _next(context);
+        }
+
+        private async Task LogRequest(HttpContext context)
+        {
+            try
+            {
+                if (context.Request.ContentLength > 0)
+                {
+                    context.Request.EnableBuffering();
+
+
+                    var buffer = new byte[context.Request.ContentLength.Value];
+                    await context.Request.Body.ReadAsync(buffer, 0, buffer.Length);
+                    var bodyAsText = Encoding.UTF8.GetString(buffer);
+                    _logger.LogInformation("Request logger");
+                    _logger.LogInformation(bodyAsText);
+
+                    context.Request.Body.Position = 0;
+                }
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError(ex.Message, ex.StackTrace);
+            }
+
+        }
+    }
+}
--- src/OzonEdu.StockApi/Program.cs
+++ src/OzonEdu.StockApi/Program.cs
@@ -1,11 +1,16 @@
+using OzonEdu.StockApi.Configuration.Fillters;
+using OzonEdu.StockApi.Configuration.Middlewares;
+using OzonEdu.StockApi.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(optionals => optionals.Filters.Add<GlobalExceptionFilter>());
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<IStockService, StockService>();
 
 var app = builder.Build();
 
@@ -15,6 +20,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseMiddleware<RequestLoggingMiddleware>();
 
 app.UseHttpsRedirection();
 
