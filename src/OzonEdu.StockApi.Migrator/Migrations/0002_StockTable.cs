d3ca2ef36eeabb52647292e64d18b8a64470fede
Илья Емец <suoza089@gmail.com>
2025-04-30 20:15:51 +0300

    Добавил миграции

---
+++ src/OzonEdu.StockApi.Migrator/Migrations/0002_StockTable.cs
@@ -0,0 +1,23 @@
+﻿using FluentMigrator;
+
+namespace OzonEdu.StockApi.Migrator.Migrations
+{
+	[Migration(2)]
+	public class StockTable : Migration
+	{
+		public override void Down()
+		{
+			Delete.Table("stocks");
+		}
+
+		public override void Up()
+		{
+			Create
+				.Table("stocks")
+				.WithColumn("id").AsInt64().Identity().PrimaryKey()
+				.WithColumn("sku_id").AsInt64().NotNullable()
+				.WithColumn("quantity").AsInt32().NotNullable()
+				.WithColumn("minimal_quantity").AsInt32().NotNullable();
+		}
+	}
+}
